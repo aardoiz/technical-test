ca8fa1f74c8090e5bc360faa068de081ed8bf6b4
aardoiz <aardoiz14@gmail.com>
2024-07-31 18:43:31 +0200

    :zap: Last touches to make it funcional

--- src/chat/search/qdrant.py
+++ src/chat/search/qdrant.py
@@ -4,12 +4,12 @@ from openai.types.embedding import Embedding
 from qdrant_client import QdrantClient
 
 from src.settings.settings import Settings
-from src.chat.domain.searcher.searcher_models import Document, SearchResponse
+from src.chat.domain.searcher.searcher_models import Document, SearchResponse, Source
 
 
 class QdrantSearcher:
     def __init__(self, settings: Settings):
-        self.client = QdrantClient(host="localhost", port=6333)
+        self.client = QdrantClient(host=settings.QDRANT_HOST, port=settings.QDRANT_PORT)
 
     def search(self, query_emb: Embedding) -> SearchResponse:
         query_emb: list[float] = query_emb.data[0].embedding
@@ -17,17 +17,18 @@ class QdrantSearcher:
 
         search_result = self.client.search(
             collection_name="LangChain-Docs", query_vector=query_emb, limit=1
-        )
+        )[0]
 
         out = [
             Document(
-                content=search_result.payload["text"],
+                content=search_result.payload["content"],
                 title=search_result.payload["title"],
-                chunk_id=1,
+                chunk_id="1",
                 score=1,
             )
         ]
 
+        sources = [Source(title=search_result.payload["title"], score=1)]
         end_time = time()
 
-        return SearchResponse(documents=out, time=end_time - init_time)
+        return SearchResponse(documents=out, time=end_time - init_time, sources=sources)
