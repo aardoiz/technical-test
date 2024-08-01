83df447a4bfee04bc8422294782974ea6a854b30
aardoiz <aardoiz14@gmail.com>
2024-08-01 17:56:01 +0200

    :recycle: Make chat_id a must

--- apps/chat/http/schemas/models.py
+++ apps/chat/http/schemas/models.py
@@ -5,7 +5,7 @@ from src.chat.domain.chat.chat_models import ChatMessage
 
 class AssistantInput(BaseModel):
     query: str
-    chat_id: str = ""
+    chat_id: str
 
 
 class AssistantOutput(BaseModel):
